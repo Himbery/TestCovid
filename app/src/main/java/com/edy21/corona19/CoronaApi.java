24443106b9214924993f9028b4c0f63cb5cde5ac
Himbery <robisl369@gmail.com>
2020-08-26 01:46:25 +0700

    second

--- app/src/main/java/com/edy21/corona19/CoronaApi.java
+++ app/src/main/java/com/edy21/corona19/CoronaApi.java
@@ -1,20 +1,16 @@
 package com.edy21.corona19;
 
-import com.edy21.corona19.Model.Coronamodel;
 import com.edy21.corona19.Model.CountriesModel;
 import com.edy21.corona19.Model.Global;
-
 import java.util.List;
-
-import retrofit2.Call;
+import io.reactivex.Observable;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
 
 public interface CoronaApi {
 
     @GET("/countries")
-    Call<List<CountriesModel>> call();
+    Observable<List<CountriesModel>> call();
 
     @GET("/all")
-    Call<Global> callGlobal();
+    Observable<Global> callGlobal();
 }
